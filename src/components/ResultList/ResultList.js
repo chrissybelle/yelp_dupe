5b79782ba0e53c1df349fc9278570fa7348b4980
cbelle <christabelle.chien@gmail.com>
2019-02-01 17:12:56 -0500

    images

--- src/components/ResultList/ResultList.js
+++ src/components/ResultList/ResultList.js
@@ -5,14 +5,20 @@ import Business from "../Business/Business";
 class ResultList extends React.Component {
     render() {
         return (
-            <div className="ResultList">
-                {!this.props.noResults ?                 
-                    this.props.businesses.map(business => {
-                        return <Business business={business} key={business.id} />;
-                    })
+            <div>
+                {!this.props.noResults ?
+
+                    <div className="ResultList">
+                        {this.props.businesses.map(business => {
+                            return <Business business={business} key={business.id} />
+                        })
+                        }
+                    </div>
                     :
-                    <h2 className="no-results">Sorry, no results found.</h2>
-                } 
+                    <div className="ResultList">
+                        <h2 className="no-results">Sorry, no results found.</h2>
+                    </div>
+                }
             </div>
         );
     }
